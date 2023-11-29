78c6fda9c63182ebb809dca5105f206fca1afe14
Bharath-boop <bharathboopathi51550@gmail.com>
2023-11-30 02:02:33 +0530

    update style

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const div=document.createElement("div");
-div.setAttribute("class","container")
+const div = document.createElement("div");
+div.setAttribute("class", "container col-sm-12")
 fetch("https://restcountries.com/v3.1/all").then((data) => data.json()).then((ele) => {
     for (let i = 0; i < ele.length; i++) {
         const div1 = document.createElement("div")
-        div1.innerHTML=`<div class="row col-lg-4 col-sm-12">
-        <div class="card h-100">
-          <img src="${"ele[i].flags.png"}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        div1.innerHTML = `<div class="card col-lg-4" id="lg">
+            <div class="card-header" id="head">${ele[i].name.common}</div>
+            <div class="card-body">
+            <img src="${ele[i].flags.png}" class="card-img-top" alt="...">
+            <h5 class="card-title ">Capital:${ele[i].capital}</h5>
+            <h5 class="card-title">Region:${ele[i].region}</h5>
+            <h5 class="card-title">Country code:${ele[i].altSpellings[0]}</h5>  
           </div>
-        </div>
-      </div>`; 
+      </div>`;
         div.append(div1)
     }
-}) 
+})
 document.body.append(div)
