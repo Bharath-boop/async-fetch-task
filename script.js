cb303c848e1ef8d1630a3b4e56a8affed353c339
Bharath-boop <bharathboopathi51550@gmail.com>
2023-11-30 22:53:27 +0530

    updated code

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const div = document.createElement("div");
-div.setAttribute("class", "container col-sm-12")
+div.setAttribute("class", "container");
+const div1 = document.createElement("div");
+div1.setAttribute("class", "row");
 fetch("https://restcountries.com/v3.1/all").then((data) => data.json()).then((ele) => {
-    for (let i = 0; i < ele.length; i++) {
-        const div1 = document.createElement("div")
-        div1.innerHTML = `<div class="card col-lg-4" id="lg">
-            <div class="card-header" id="head">${ele[i].name.common}</div>
-            <div class="card-body">
-            <img src="${ele[i].flags.png}" class="card-img-top" alt="...">
-            <h5 class="card-title ">Capital:${ele[i].capital}</h5>
-            <h5 class="card-title">Region:${ele[i].region}</h5>
-            <h5 class="card-title">Country code:${ele[i].altSpellings[0]}</h5>  
-          </div>
-      </div>`;
-        div.append(div1)
+    for (i = 0; i < ele.length; i++) {
+        const res = document.createElement("div")
+        res.setAttribute("class", "col-sm-4")
+        res.innerHTML = `<div class="card-header ">
+        <h1 class="text-center" id="title">${ele[i].name.common} </h1>
+        </div> 
+        <img src="${ele[i].flags.png}" class="card-img-top" alt="...">
+        <div class="card-body id="cbody">
+        <h5 class="card-title"> Captial : ${ele[i].capital}</h5>
+        <h5 class="card-title">Region : ${ele[i].region}</h5>
+        <h5 class="card-title">Lat : ${ele[i].latlng[0]}</h5>
+        <h5 class="card-title">Lng : ${ele[i].latlng[1]}</h5>
+        <h5 class="card-title">Country Code : ${ele[i].car.signs[0]}</h5>
+        </div>
+        <button type="button" class="btn btn-primary"">Click for Weather</button>`
+        div1.append(res);
     }
 })
-document.body.append(div)
+div.append(div1);
+document.body.append(div);
